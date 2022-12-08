5e4869f273ce8ae54ba9e170be964116857dded6
Abdul Ahad bhatti <110728909+ahadb802@users.noreply.github.com>
2022-12-08 19:41:43 +0500

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import createTodo from './modules/displayTask.js';
-import addTasks from './modules/addTodoTask.js';
-import toDoItemStatus from './modules/clearAll.js';
-
-window.onload = createTodo();
-addTasks();
-toDoItemStatus();
--- src/modules/TodoMagazine.js
+++
@@ -1,6 +0,0 @@
-const TodoMagazine = (todoList) => {
-  todoList.sort((a, b) => a.index - b.index);
-  localStorage.setItem('todoList', JSON.stringify(todoList));
-};
-
-export default TodoMagazine;
